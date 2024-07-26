e65bad6b09ded43afec654ca80cf44effa776b30
Enrique Sabariego <quiquesaba@gmail.com>
2024-07-26 18:29:50 +0200

    end 1

--- src/heroes/pages/SearchHero.jsx
+++ src/heroes/pages/SearchHero.jsx
@@ -3,8 +3,6 @@ import { useForm } from '../../hooks/useForm'
 import queryString from 'query-string'
 import { getHeroByName } from '../helpers';
 import { HeroCard } from '../components/HeroCard';
-// import { heroes } from '../data/heroes';
-// import { HeroCard } from "../components/HeroCard"
 
 export const SearchHero = () => {
 
@@ -13,6 +11,9 @@ export const SearchHero = () => {
 
   const { q = '' } = queryString.parse( location.search );
   const heroes = getHeroByName(q);
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
 
   const { searchText, onInputChange} = useForm({
     searchText: q
@@ -54,13 +55,24 @@ export const SearchHero = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-           
-          <div className="alert alert-primary">
+          
+          {/* { 
+            ( q === '' )
+              ? <div className="alert alert-primary">Search a hero</div>
+              : ( heroes.length === 0 ) && <div className="alert alert-danger">No hero with <b>{q}</b></div>  
+          } */}
+
+          <div className='alert alert-primary animate__animated animate__fadeIn'
+              style={{ display: showSearch ? '' : 'none' }}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">No hero with <b>{q}</b></div>
-          
+          <div className='alert alert-danger animate__animated animate__fadeIn'
+              style={{ display: showError ? '' : 'none' }}>
+            No hero with <b>{q}</b>
+          </div>
+        
+         
           {
             heroes.map(hero => (
               <HeroCard key={hero.id} {...hero}/>
